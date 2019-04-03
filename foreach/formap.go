4e00becfdebcb37817c4760116f4ad62409e751b
kwanhur <huang_hua2012@163.com>
2019-04-04 00:02:26 +0800

    rectify foreach slice

--- foreach/formap.go
+++ foreach/formap.go
@@ -16,6 +16,10 @@ stu:n1 ptr:0xc42000a180 val:{Age:10 Name:n1}
 stu:n2 ptr:0xc42000a180 val:{Age:12 Name:n2}
 stu:n3 ptr:0xc42000a180 val:{Age:13 Name:n3}
 m after: map[n1:0xc420092120 n2:0xc420092120 n3:0xc420092120]
+stu:n1 ptr:0xc420098140 val:{Age:10 Name:n1}
+stu:n2 ptr:0xc420098158 val:{Age:12 Name:n2}
+stu:n3 ptr:0xc420098170 val:{Age:13 Name:n3}
+m rectify: map[n1:0xc420098140 n2:0xc420098158 n3:0xc420098170]
 */
 func ParseStudent() {
 	m := make(map[string]*Student)
@@ -35,4 +39,10 @@ func ParseStudent() {
 	}
 	fmt.Printf("m after: %+v\n", m)
 
+	// should write it like this
+	for i := 0; i < len(stus); i++ {
+		fmt.Printf("stu:%s ptr:%p val:%+v\n", stus[i].Name, &stus[i], stus[i])
+		m[stus[i].Name] = &stus[i]
+	}
+	fmt.Printf("m rectify: %+v\n", m)
 }
