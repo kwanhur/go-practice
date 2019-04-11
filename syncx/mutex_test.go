7a6c82b9f609127b45e01a56b89f2b577f758504
HUANGHUA670 <huanghua670@pingan.com.cn>
2019-04-11 09:15:54 +0800

    mutex combine test

--- syncx/mutex_test.go
+++ syncx/mutex_test.go
@@ -1,1 +1,16 @@
 package syncx
+
+import "testing"
+
+func TestUserAges_Add(t *testing.T) {
+	u := UserAges{
+		ages: make(map[string]int),
+	}
+	defer func() {
+		if err := recover(); err != nil {
+			t.Error(err)
+		}
+	}()
+	u.Add("kwanhur", 20)
+	t.Log(u.Get("kwanhur"))
+}
